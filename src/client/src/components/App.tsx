b906281725122aca601cbd00e9410be787c92f11
Lenny Hyungsuk Kim <lkim.hyungsuk@gmail.com>
2023-08-27 17:39:57 -0700

    (remove) unused code

---
+++ src/client/src/components/App.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState({
+    message: "Loading...",
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:4000/testApi");
+      const data = await response.json();
+      setData(data);
+      console.log("From React, data: ", data);
+    }
+    fetchData();
+  }, []);
+
+  return <div className="App">Here is the fetched data: {data?.message}</div>;
+}
+
+export default App;
